140f67a6d9cf89bd55bda0390f856e2722035efb
evgenyml <31263580+evgenyml@users.noreply.github.com>
2017-11-12 02:28:10 +0800

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+$author = $_SESSION['login_user'];
+
+if (isset($_POST['submit'], $_GET['pid'])) {
+	$id = $_GET['pid'];
+	$comment = $_POST['comment'];
+	$today = date("Y-m-d");
+	
+	$cxn = mysqli_connect('localhost', 'root', '', 'shopsmart') or die ('Could not connect');
+	$query = mysqli_query($cxn, "INSERT INTO comment(author, productID, comment, date) VALUES('$author', 8,'$comment', '$today')");
+	
+	mysqli_close($cxn);
+	
+	header("Location: product.php?pid=$id");
+}
+
+
+
+?>
